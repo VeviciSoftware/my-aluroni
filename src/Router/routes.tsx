6162256bc34137a29b289aa98f9e9eb56eea30e6
felipe <felipeadar108@gmail.com>
2024-12-19 16:26:27 -0300

    :recycle: refactor: aplicando react lazy no restante dos componentes

--- src/Router/routes.tsx
+++ src/Router/routes.tsx
@@ -1,28 +1,33 @@
-import Inicio from '../pages/Inicio';
-import Cardapio from '../pages/Cardapio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Menu from '../components/Menu';
-import MainPage from '../components/MainPage';
-import Sobre from '../pages/Sobre';
 import Footer from '../components/Footer';
-import NotFound from '../pages/NotFound';
-import Prato from '../pages/Prato';
+import { lazy, Suspense } from 'react';
+
+const Inicio = lazy(() => import('../pages/Inicio'));
+const Cardapio = lazy(() => import('../pages/Cardapio'));
+const MainPage = lazy(() => import('../components/MainPage'));
+const Sobre = lazy(() => import('../pages/Sobre'));
+const Prato = lazy(() => import('../pages/Prato'));
+const NotFound = lazy(() => import('../pages/NotFound'));
+
 
 export default function AppRouter() {
     return (
         <main className='container'>
             <Router>
-                <Menu />
-                <Routes>
-                    <Route path='/' element ={<MainPage/>}>
-                        <Route index element={<Inicio />} />
-                        <Route path='cardapio' element={<Cardapio />} />
-                        <Route path='sobre' element={<Sobre />} />
-                        <Route path='prato/:id' element={<Prato />} />
-                    </Route>
-                    <Route path='*' element={<NotFound />} />
-                </Routes>
-                <Footer />
+                <Suspense fallback={<p> Carregando... </p>}>
+                    <Menu />
+                    <Routes>
+                        <Route path='/' element={<MainPage />}>
+                            <Route index element={<Inicio />} />
+                                <Route path='cardapio' element={<Cardapio />} />
+                                <Route path='sobre' element={<Sobre />} />
+                                <Route path='prato/:id' element={<Prato />} />
+                        </Route>
+                        <Route path='*' element={<NotFound />} />
+                    </Routes>
+                    <Footer />
+                </Suspense>
             </Router>
         </main>
     );
